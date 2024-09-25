4795444af30b8e535696de58ecc4b9e43a217cd0
Fabri Cardozo <fabricardozo271003@gmail.com>
2024-09-25 20:35:03 -0300

    ejercicio 3 terminado

--- src/components/home/recents-songs.tsx
+++ src/components/home/recents-songs.tsx
@@ -1,29 +1,35 @@
 import styles from '../../styles/recent-song.module.css'
-
-const recientes = [
-    {imagen : 'https://cdns-images.dzcdn.net/images/cover/04a95dd06db9a2941a47b3c730b34a7d/1900x1900-000000-80-0-0.jpg', album : 'Prestigio', autor : 'Daddy Yankee'  },
-    {imagen : 'https://i.scdn.co/image/ab67616d0000b273a02702f77600ef7f377192e2', album : 'En Pleno Vuelo', autor : 'Marco Antonio Solis'  },
-    {imagen : 'https://www.rockaxis.com/img/newsList/2525753.jpg', album : 'Jessico', autor : 'Babasonicos'  },
-    {imagen : 'https://indiehoy.com/wp-content/uploads/2023/10/babasonicos-infame.jpg', album : 'Infame', autor : 'Babasonicos'  },
-    {imagen : 'https://m.media-amazon.com/images/I/81JM2ijH7tL._UF1000,1000_QL80_.jpg', album : 'Infesstisuman', autor : 'Ghost'  },
-];
+import useFetch from '../../hooks/custom-hooks/useFetch'
+import ReproducirPodcast from './podcast-play-pause';
 
 function ListSongs() {
-    return (
-            <div className={styles.contenedor}>
-                {recientes.map((reciente) => {
-                    return( 
-                        <div className="container">
-                            <img className={styles.imagen} src={reciente.imagen} />  
-                            <p className={styles.album}>{reciente.album}</p>
-                            <p className={styles.aut}>{reciente.autor}</p>  
-                        </div>
-                    )
-                } )}
-                <div/>
-            </div>   
-    );
-}
     
+    const { info, loading, error} = useFetch();
+
+
+    return (
+        <div className={styles.contenedor}>
+            <ul>
+                {loading && <h3>El contenido se está cargando... Por favor espere.</h3>}
+                {error && <h1>{error}</h1>}
+                {info.map((podcast) => {
+                    return <li key={podcast.id}>
+                        <div className={styles.podcast}>
+                            <img src={podcast.channel.urls.logo_image.original} alt="" />
+                            <div className={styles.title}>
+                                <p>{podcast.title}</p>
+                            </div>
+                            <p>{podcast.description}</p>
+                            
+                            <ReproducirPodcast audioSrc={podcast.urls.high_mp3}/>
+
+                        </div>
+                    </li>
+                }).slice(0, 8)}
+            </ul>
+        </div>
+    )
+}
+
 
 export default ListSongs;
--- src/components/home/similar-songs.tsx
+++ src/components/home/similar-songs.tsx
@@ -1,26 +1,10 @@
 import styles from '../../styles/similar-songs.module.css'
-const escucharDeNuevo = [
-    {imagen : 'https://cdns-images.dzcdn.net/images/cover/04a95dd06db9a2941a47b3c730b34a7d/1900x1900-000000-80-0-0.jpg', accion : 'Volver a escuchar', reproducciones : '54.2M oyentes mensuales'  },
-    {imagen : 'https://i.scdn.co/image/ab67616d0000b273a02702f77600ef7f377192e2', accion : 'Volver a escuchar', reproducciones : '10.7M oyentes mensuales'  },
-    {imagen : 'https://www.rockaxis.com/img/newsList/2525753.jpg', accion : 'Volver a escuchar', reproducciones : '3.8M oyentes mensuales'  },
-    {imagen : 'https://indiehoy.com/wp-content/uploads/2023/10/babasonicos-infame.jpg', accion : 'Volver a escuchar', reproducciones : '3.8M oyentes mensuales'  },
-    {imagen : 'https://m.media-amazon.com/images/I/81JM2ijH7tL._UF1000,1000_QL80_.jpg', accion : 'Volver a escuchar', reproducciones : '7.8M oyentes mensuales'  },
-];
+import ListSongs from './recents-songs';
+
 
 function SimilarSongs() {
     return (
-            <div className={styles.contenedor}>
-                {escucharDeNuevo.map((escuchar) => {
-                    return( 
-                        <div className="container">
-                            <img className={styles.imagen} src={escuchar.imagen} />  
-                            <p className={styles.accion}>{escuchar.accion}</p>
-                            <p className={styles.aut}>{escuchar.reproducciones}</p>  
-                        </div>
-                    )
-                } )}
-                <div/>
-            </div>   
+        <ListSongs/>  
     );
 }
     
